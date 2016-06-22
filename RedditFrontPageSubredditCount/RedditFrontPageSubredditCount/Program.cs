4116cab6ce90f733a3de089ac5f2d5041ba23b0a
aethius <heyitsamp@gmail.com>
2016-06-22 13:46:12 -0400

    Uploading Program Files
    The first commit of all the program files.

---
+++ RedditFrontPageSubredditCount/RedditFrontPageSubredditCount/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedditFrontPageSubredditCount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WebClient client = new WebClient();
+            string reply = client.DownloadString("http://reddit.com/r/all");
+
+            string findPoint = "data-subreddit=\"";
+            //string findPoint = "goofygoober";
+
+            // For seeing the entire html grabbed from the source
+            // Console.WriteLine(reply);
+            // File.WriteAllText(@"C:\users\anthony\documents\C#\redditfrontpage.txt", reply);
+            int count = 0;
+            do
+            {
+                count++;
+                int first = reply.IndexOf(findPoint) + findPoint.Length;
+               // Console.WriteLine(reply.IndexOf(findPoint));
+                reply = reply.Substring(first);
+
+                int second = reply.IndexOf("\"");
+
+                string subreddit = reply.Substring(0, second);
+
+                Console.Write(count + " ");
+                Console.WriteLine(subreddit);
+
+            } while (reply.IndexOf(findPoint) != -1 );
+
+
+
+
+
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
